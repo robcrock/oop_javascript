1e2d6d16845d49e131f92653e25fe871d11fb928
robcrock <rob@robcrock.com>
2018-02-05 23:29:48 +1100

    More indepth exploration to the Object prototype.

--- app.js
+++ app.js
@@ -1,14 +1,31 @@
-function Person(name, dob) {
-  this.name = name;
-  // this.age = age;
+function Person(firstName, lastName, dob) {
+  this.firstName = firstName;
+  this.lastName = lastName;
   this.birthday = new Date(dob);
-  this.calculateAge = function() {
-    const diff = Date.now() - this.birthday.getTime();
-    const ageDate = new Date(diff);
-    return Math.abs(ageDate.getUTCFullYear() - 1970)
-  }
 }
 
-const robert = new Person('Robert', '05-11-1989');
+// Calculate age
+Person.prototype.calculateAge = function () {
+  const diff = Date.now() - this.birthday.getTime();
+  const ageDate = new Date(diff);
+  return Math.abs(ageDate.getUTCFullYear() - 1970)
+};
 
-console.log(robert.calculateAge());
+// Get fullname
+Person.prototype.getFullName = function () {
+  return `${this.firstName} ${this.lastName}`;
+};
+
+// Mary get married
+Person.prototype.getsMarried = function (newLastName) {
+  this.lastName = newLastName;
+};
+
+const john = new Person('John', 'Doe', '05-11-1989');
+const mary = new Person('Mary', 'Johnson', 'March 20 1978');
+
+console.log(mary.getFullName());
+
+mary.getsMarried('Smith');
+
+console.log(mary.getFullName());
